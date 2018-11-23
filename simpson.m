f09e796eca843e34f98ca8136f55b4b4ef560db5
Naomi M. Honti <naomiperla.macias@hotmail.com>
2018-11-22 21:23:00 -0600

    Update simpson.m

--- simpson.m
+++ simpson.m
@@ -1,23 +1,23 @@
 %Parameters: function, min, max, segments
-%output    : integration, vector, message 
+%output    : integration, vector, message
 function [varargout] = simpson(eq, min, max, sgm)
-    f = matlabFunction(evalin(symengine, eq));
-    h = (max-min)/sgm;
-    vX = min:h:max;
-    eq2 = 0;
-    for i=2:sgm
-        if(mod(i,3) == 1)
-           eq2 = eq2 + 2*f(vX(i));
-        else
-           eq2 = eq2 + 3*f(vX(i));
-        end
-        display(eq2);
+f = matlabFunction(evalin(symengine, eq));
+h = (max-min)/sgm;
+vX = min:h:max;
+eq2 = 0;
+for i=2:sgm
+    if(mod(i,3) == 1)
+        eq2 = eq2 + 2*f(vX(i));
+    else
+        eq2 = eq2 + 3*f(vX(i));
     end
-    vY = f(vX);
-    I = 3*h*(f(vX(1))+eq2+f(vX(sgm+1)))/8;
-    realI = integral(f,min,max);
-    varargout{1} = I;
-    varargout{2} = (realI-I)/realI;
-    varargout{3} = [vX;vY];
-    varargout{4} = 'Success!';
+    display(eq2);
+end
+vY = f(vX);
+I = 3*h*(f(vX(1))+eq2+f(vX(sgm+1)))/8;
+realI = integral(f,min,max);
+varargout{1} = I;
+varargout{2} = (realI-I)/realI;
+varargout{3} = [vX;vY];
+varargout{4} = 'Success!';
 end
