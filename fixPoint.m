ec7e4bd895d1245b52941c3fee38d25f3bb1ea34
Naomi M. Honti <naomiperla.macias@hotmail.com>
2018-11-22 21:21:04 -0600

    Update fixPoint.m

--- fixPoint.m
+++ fixPoint.m
@@ -1,30 +1,30 @@
 %Parameters: handle, initialValue, #iteretions
 %output    : root, ea, vector, message
 function [varargout] = fixPoint(h, iv, it)
-    handle = matlabFunction(evalin(symengine, h));
-    Xp = iv;
-    is_root = false;
-    counter = 1;
-    ea = 0;
-    varargout{3} = [];
-    while(counter <= it)
-        X = handle(Xp);
-        if(counter > 1)
-            ea = abs((X-Xp)/X)*100;
-            varargout{3} = [varargout{3} ea];
-        end
-        if(round(X,4)==round(Xp,4))
-            varargout{1} = X;
-            counter = it;
-            is_root = true;
-            varargout{4} = 'Success!';
-        end
-        varargout{2} = ea;
-        Xp = X;
-        counter = counter + 1;
+handle = matlabFunction(evalin(symengine, h));
+Xp = iv;
+is_root = false;
+counter = 1;
+ea = 0;
+varargout{3} = [];
+while(counter <= it)
+    X = handle(Xp);
+    if(counter > 1)
+        ea = abs((X-Xp)/X)*100;
+        varargout{3} = [varargout{3} ea];
     end
-    if(~is_root)
+    if(round(X,4)==round(Xp,4))
         varargout{1} = X;
-        varargout{4} = 'Aproximate answer.';
+        counter = it;
+        is_root = true;
+        varargout{4} = 'Success!';
     end
+    varargout{2} = ea;
+    Xp = X;
+    counter = counter + 1;
+end
+if(~is_root)
+    varargout{1} = X;
+    varargout{4} = 'Aproximate answer.';
+end
 end
